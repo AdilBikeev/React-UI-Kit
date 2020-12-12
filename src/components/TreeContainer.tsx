6d9c624f35d1ef796435f6f4cab4bc8353a77a9b
AdCoder <bikeev1999@mail.ru>
2020-12-13 01:45:25 +0300

    Правки в отображении узло дерева

--- src/components/TreeContainer.tsx
+++ src/components/TreeContainer.tsx
@@ -10,10 +10,13 @@ interface Props {
 export const TreeContainer: React.FC<Props> = ({
     subNodes
 }) => {
+    const countSubNodes = subNodes ? subNodes.length : 0 
 
     return (<ul className={classes.Container}>
         {subNodes?.map((node, index) => (<TreeNode key={index}
                                                   index={index}
+                                                  isLeaf={!node.subNodes || node.subNodes.length === 0}
+                                                  isLast={index === countSubNodes - 1}
                                                   {...node} />)
             )
         }
--- src/components/TreeNode.tsx
+++ src/components/TreeNode.tsx
@@ -6,6 +6,8 @@ import { TreeContainer } from './TreeContainer';
 
 interface Props {
     index: number,
+    isLeaf: boolean,
+    isLast: boolean,
 }
 
 export interface TreeNodeObj {
@@ -15,12 +17,16 @@ export interface TreeNodeObj {
 
 /**
  * Узел дерева
- * @param key Индекс узла
+ * @param index Индекс узла
+ * @param isLeaf Указывает, что узел является листом 
+ * @param isLast Указывает, что узел является последним
  * @param content Контент узла
  * @param subNodes Дочерние узлы
  */
 export const TreeNode: React.FC<TreeNodeObj & Props> = ({
     index,
+    isLeaf,
+    isLast,
     content,
     subNodes
 }) => {
@@ -28,14 +34,14 @@ export const TreeNode: React.FC<TreeNodeObj & Props> = ({
     /* TODO: Поменять на единый обработчик для всех элементов через Redux или Effector  */
     const [isExpanded, setExpanded] = useState(false)
     const isRoot = index === 0
-    const isLeaf = !subNodes || subNodes.length === 0
 
     return (<li className={clsx(classes.Node, {
-        [classes.isRoot]: isRoot,
+        [classes.IsRoot]: isRoot,
         [classes.ExpandClosed]: !isLeaf && !isExpanded,
         [classes.ExpandOpen]: !isLeaf && isExpanded,
         [classes.ExpandLeaf]: isLeaf,
-        [classes.IsLast]: isLeaf,
+        [classes.IsLast]: isLeaf && isLast,
+        [classes.IsMiddle]: isLeaf && !isLast,
     })}>
         <div className={classes.Expand} onClick={() => setExpanded(!isExpanded)}></div>
         <div className={classes.Content}>{content}</div>
